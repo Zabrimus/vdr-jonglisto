6e08fc15bda39b1f31f5c5195964ab40d1a193f2
Zabrimus <github@hannebauer.org>
2016-12-22 21:41:02 +0100

    implemented some methods

--- src/main/java/vdr/jonglisto/web/components/ChannelConfig.java
+++ src/main/java/vdr/jonglisto/web/components/ChannelConfig.java
@@ -5,14 +5,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.apache.tapestry5.Link;
 import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.ajax.AjaxResponseRenderer;
+import org.apache.tapestry5.services.ajax.JavaScriptCallback;
 import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 import org.slf4j.Logger;
 
@@ -20,7 +24,8 @@ import vdr.jonglisto.lib.model.Channel;
 import vdr.jonglisto.lib.util.FilterEncrypted;
 import vdr.jonglisto.lib.util.FilterRadioTv;
 
-@Import(library = { "webjars:jquery-ui:$version/jquery-ui.js" }, stylesheet = {"webjars:jquery-ui:$version/jquery-ui.css" })
+@Import(library = { "webjars:jquery-ui:$version/jquery-ui.js" }, stylesheet = {
+        "webjars:jquery-ui:$version/jquery-ui.css" })
 public class ChannelConfig extends BaseComponent {
 
     @Inject
@@ -32,6 +37,15 @@ public class ChannelConfig extends BaseComponent {
     @Inject
     protected JavaScriptSupport javaScriptSupport;
 
+    @InjectComponent
+    private Zone channelConfigZone;
+
+    @InjectComponent
+    private Zone groupRenameZone;
+
+    @InjectComponent
+    private Zone newGroupZone;
+
     @Inject
     protected Messages messages;
 
@@ -39,9 +53,11 @@ public class ChannelConfig extends BaseComponent {
     @Persist
     private List<String> channelGroups;
 
+    @Persist
     @Property
     private String group;
 
+    @Persist
     @Property
     private Channel channel;
 
@@ -57,9 +73,17 @@ public class ChannelConfig extends BaseComponent {
     @Persist
     private Map<String, List<Channel>> channelsInGroup;
 
+    @Property
+    @Persist
+    private String parkingGroup;
+
+    @Property
+    private String newGroupName;
+    
     void setupRender() {
         channelsInGroup = new HashMap<>();
         channelGroups = new ArrayList<>();
+        parkingGroup = "-- " + messages.get("parking_group") + " --";
 
         Optional<List<String>> gr = vdrDataService.getGroups(currentVdrView.getChannelVdr().get());
         if (gr.isPresent()) {
@@ -68,11 +92,14 @@ public class ChannelConfig extends BaseComponent {
                     vdrDataService.getChannelsInGroup(currentVdrView.getChannelVdr().get(), g, true).get()));
         } else {
             // create a default group
-            channelGroups.add("DEFAULT");
-            channelsInGroup.put("DEFAULT",
+            channelGroups.add("-- Default --");
+            channelsInGroup.put("-- Default --",
                     vdrDataService.getChannels(currentVdrView.getChannelVdr().get(), true).get());
         }
-        
+
+        channelGroups.add(parkingGroup);
+        channelsInGroup.put(parkingGroup, new ArrayList<>());
+
         filterRadio = FilterRadioTv.RADIOTV;
         filterEncrypted = FilterEncrypted.ALL;
     }
@@ -94,14 +121,15 @@ public class ChannelConfig extends BaseComponent {
         String fromidx = request.getParameter("fromidx");
         String toidx = request.getParameter("toidx");
         String group = request.getParameter("group");
-        
-        System.err.println("id: " + id + ", to: " + to + ", from: " + from + ", fromidx: " + fromidx + ", toidx: " + toidx + ", group: " + group);
+
+        System.err.println("id: " + id + ", to: " + to + ", from: " + from + ", fromidx: " + fromidx + ", toidx: "
+                + toidx + ", group: " + group);
     }
 
     public void onSaveChannelsConf() {
         log.info("Currently not implemented: ChannelConfig.onSaveChannelsConf");
     }
-    
+
     public void onLoadChannelsConf() {
         log.info("Currently not implemented: ChannelConfig.onLoadChannelsConf");
     }
@@ -109,36 +137,132 @@ public class ChannelConfig extends BaseComponent {
     public void onCreateChannelsConf() {
         log.info("Currently not implemented: ChannelConfig.onCreateChannelsConf");
     }
-    
-    public void onDeleteObsolete() {        
+
+    public void onDeleteObsolete() {
         log.info("Currently not implemented: ChannelConfig.onDeleteObsolete");
     }
-    
+
     public void onFixObsolete() {
         log.info("Currently not implemented: ChannelConfig.onFixObsolete");
     }
-    
-    public void onCreateChannelGroup() {        
-        log.info("Currently not implemented: ChannelConfig.onCreateChannelGroup");
-    }       
+
+    public void onCreateChannelGroup() {
+        if (request.isXHR()) {
+            ajaxResponseRenderer.addCallback(new JavaScriptCallback() {
+                public void run(JavaScriptSupport javascriptSupport) {
+                    javaScriptSupport.require("dialogmodal").invoke("activate").with("newGroup");
+                }
+            });
+            ajaxResponseRenderer.addRender(newGroupZone);
+        }
+    }
 
     public void onChangeFromFilterRadio() {
         log.info("Currently not implemented: ChannelConfig.onChangeFromFilterRadio");
     }
-    
+
     public void onChangeFromFilterEncrypted() {
         log.info("Currently not implemented: ChannelConfig.onChangeFromFilterEncrypted");
     }
 
-    public void onRenameGroup() {        
-        log.info("Currently not implemented: ChannelConfig.onRenameGroup");
+    public void onRenameGroup(String selectedGroup) {
+        if (selectedGroup.startsWith("--")) {
+            // internal group, do nothing
+            return;
+        }
+        
+        if (request.isXHR()) {
+            group = selectedGroup;
+            ajaxResponseRenderer.addCallback(new JavaScriptCallback() {
+                public void run(JavaScriptSupport javascriptSupport) {
+                    javaScriptSupport.require("dialogmodal").invoke("activate").with("groupRename");
+                }
+            });
+            ajaxResponseRenderer.addRender(groupRenameZone);
+        }
+    }
+
+    public void onDeleteGroup(String selectedGroup) {
+        if (selectedGroup.startsWith("--")) {
+            // internal group, do nothing
+            return;
+        }
+
+        List<Channel> removed = channelsInGroup.get(selectedGroup);
+        channelsInGroup.remove(selectedGroup);
+        channelsInGroup.get("-- " + messages.get("parking_group") + " --").addAll(removed);
+
+        channelGroups.remove(selectedGroup);
+
+        updateZone();
+    }
+
+    public void onSortGroup(String selectedGroup) {
+        channelsInGroup.put(selectedGroup,
+                channelsInGroup.get(selectedGroup) //
+                        .stream() //
+                        .sorted((x, y) -> x.getName().toUpperCase().compareTo(y.getName().toUpperCase())) //
+                        .collect(Collectors.toList()));
+
+        updateZone();
+    }
+
+    public void onSuccessFromNewGroupForm() {
+        channelGroups.add(newGroupName);
+        channelsInGroup.put(newGroupName, new ArrayList<>());
+        
+        if (request.isXHR()) {
+            ajaxResponseRenderer.addCallback(new JavaScriptCallback() {
+                public void run(JavaScriptSupport javascriptSupport) {
+                    javaScriptSupport.require("dialogmodal").invoke("hide").with("newGroup");
+                }
+            });
+        }
+        
+        updateZone();
     }
     
-    public void onDeleteGroup() {        
-        log.info("Currently not implemented: ChannelConfig.onDeleteGroup");
+    public void onSuccessFromRenameGroupForm() {
+        int idx = channelGroups.indexOf(group);
+        channelGroups.set(idx, newGroupName);
+        
+        List<Channel> ch = channelsInGroup.get(group);
+        channelsInGroup.remove(group);
+        channelsInGroup.put(newGroupName, ch);
+        
+        if (request.isXHR()) {
+            ajaxResponseRenderer.addCallback(new JavaScriptCallback() {
+                public void run(JavaScriptSupport javascriptSupport) {
+                    javaScriptSupport.require("dialogmodal").invoke("hide").with("groupRename");
+                }
+            });
+        }
+        
+        updateZone();
+    }       
+    
+    public void onCancel() {
+        if (request.isXHR()) {
+            ajaxResponseRenderer.addCallback(new JavaScriptCallback() {
+                public void run(JavaScriptSupport javascriptSupport) {
+                    javaScriptSupport.require("dialogmodal").invoke("hide").with("newGroup");
+                    javaScriptSupport.require("dialogmodal").invoke("hide").with("groupRename");
+                }
+            });
+        }
     }
     
-    public void onSortGroup() {       
-        log.info("Currently not implemented: ChannelConfig.onSortGroup");
+    private void updateZone() {
+        if (request.isXHR()) {
+            ajaxResponseRenderer.addRender(channelConfigZone).addCallback(new JavaScriptCallback() {
+
+                @Override
+                public void run(JavaScriptSupport javascriptSupport) {
+                    Link link = componentResources.createEventLink("draggedChannel");
+                    String baseURI = link.toAbsoluteURI();
+                    javaScriptSupport.require("portlet").with(baseURI);
+                }
+            });
+        }
     }
 }
